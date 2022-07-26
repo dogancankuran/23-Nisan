b9838e63f1845272bba45822651769e03d65c00b
dogancankuran <109361149+dogancankuran@users.noreply.github.com>
2022-07-26 21:27:54 +0300

    Add files via upload

---
+++ 23-Nisan/clock.js
@@ -0,0 +1,7 @@
+
+let name = prompt("Lütfen Adınızı Giriniz; ")
+let info = document.querySelector("#myName")
+info.innerHTML =  `${name}`
+
+const d = new Date();
+document.getElementById("demo").innerHTML = d;
